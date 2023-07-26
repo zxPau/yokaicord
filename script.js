18a720d10be3e2ac6ba9f5546256e082702b9e2e
Pau <140620286+zxPau@users.noreply.github.com>
2023-07-26 21:35:19 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function redirectToInvite() {
+    // Redirige al enlace de invitación del bot de Discord
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=TU_CLIENT_ID&scope=bot&permissions=PERMISOS";
+    // Reemplaza "TU_CLIENT_ID" con el ID de cliente de tu bot y "PERMISOS" con los permisos necesarios
+}
